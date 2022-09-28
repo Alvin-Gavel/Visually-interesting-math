c9a2bf154125aee0469e682cdd3b741c5514f777
Alvin-Gavel <alvin.gavel@gmail.com>
2022-09-28 13:25:20 +0200

    Fixed bug in saving

--- fractals.py
+++ fractals.py
@@ -3,9 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
 def divergence_fractal(function, plot_folder_path, x_range, y_range, x_steps = 101, y_steps = 101, iterations = 100):
-   if plot_folder_path[-1] != '/':
-      plot_folder_path += '/'
-   
+  
    # If we have symmetric intervals, we want to make sure that the real
    # and imaginary axes are plotted.
    if abs(x_range[0]) == abs(x_range[1]):
@@ -27,16 +25,34 @@ def divergence_fractal(function, plot_folder_path, x_range, y_range, x_steps = 1
       divergence_time[diverged] = np.minimum(divergence_time[diverged], i)
    return real_axis, imag_axis, divergence_time
 
-def make_multibrot(plot_folder_path, d = 2, x_range = [-2, 2], y_range = [-2, 2], x_steps = 101, y_steps = 101, iterations = 100):
+def multibrot(plot_folder_path, d = 2, x_range = [-2, 2], y_range = [-2, 2], x_steps = 101, y_steps = 101, iterations = 100):
    """
    Plots the Mandelbrot set, or a generalisation with arbitrary exponent d.
    """
+   if plot_folder_path[-1] != '/':
+      plot_folder_path += '/'
+   
    multibrot_function = lambda complex_plane : complex_plane**d
-
    real_axis, imag_axis, divergence_time = divergence_fractal(multibrot_function, plot_folder_path, x_range, y_range, x_steps = x_steps, y_steps = y_steps, iterations = iterations)
 
    plt.pcolormesh(real_axis, imag_axis, divergence_time, shading = 'nearest', cmap = 'gist_gray', norm=LogNorm())
    plt.axis('off')
    plt.savefig("{}multibrot_{}.png".format(plot_folder_path, d), format = 'png', bbox_inches = 'tight', dpi=100)
+   plt.close()
+   return
+   
+def mandelbar(plot_folder_path, d = 2, x_range = [-2, 2], y_range = [-2, 2], x_steps = 101, y_steps = 101, iterations = 100):
+   """
+   Plots the Mandelbrot set, or a generalisation with arbitrary exponent d.
+   """
+   if plot_folder_path[-1] != '/':
+      plot_folder_path += '/'
+      
+   multibrot_function = lambda complex_plane : np.conjugate(complex_plane)**d
+   real_axis, imag_axis, divergence_time = divergence_fractal(multibrot_function, plot_folder_path, x_range, y_range, x_steps = x_steps, y_steps = y_steps, iterations = iterations)
+
+   plt.pcolormesh(real_axis, imag_axis, divergence_time, shading = 'nearest', cmap = 'gist_gray', norm=LogNorm())
+   plt.axis('off')
+   plt.savefig("{}mandelbar_{}.png".format(plot_folder_path, d), format = 'png', bbox_inches = 'tight', dpi=100)
    plt.close()
    return
