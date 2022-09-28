59f176960595d3c33a84a4a905746285d2edd325
Alvin-Gavel <alvin.gavel@gmail.com>
2022-09-28 13:39:40 +0200

    Added burning ship fractal

--- fractals.py
+++ fractals.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
 def divergence_fractal(function, plot_folder_path, x_range, y_range, x_steps = 101, y_steps = 101, iterations = 100):
-  
    # If we have symmetric intervals, we want to make sure that the real
    # and imaginary axes are plotted.
    if abs(x_range[0]) == abs(x_range[1]):
@@ -32,7 +31,7 @@ def multibrot(plot_folder_path, d = 2, x_range = [-2, 2], y_range = [-2, 2], x_s
    if plot_folder_path[-1] != '/':
       plot_folder_path += '/'
    
-   multibrot_function = lambda complex_plane : complex_plane**d
+   multibrot_function = lambda z : z**d
    real_axis, imag_axis, divergence_time = divergence_fractal(multibrot_function, plot_folder_path, x_range, y_range, x_steps = x_steps, y_steps = y_steps, iterations = iterations)
 
    plt.pcolormesh(real_axis, imag_axis, divergence_time, shading = 'nearest', cmap = 'gist_gray', norm=LogNorm())
@@ -48,11 +47,27 @@ def mandelbar(plot_folder_path, d = 2, x_range = [-2, 2], y_range = [-2, 2], x_s
    if plot_folder_path[-1] != '/':
       plot_folder_path += '/'
       
-   multibrot_function = lambda complex_plane : np.conjugate(complex_plane)**d
-   real_axis, imag_axis, divergence_time = divergence_fractal(multibrot_function, plot_folder_path, x_range, y_range, x_steps = x_steps, y_steps = y_steps, iterations = iterations)
+   mandelbar_function = lambda z : np.conjugate(z)**d
+   real_axis, imag_axis, divergence_time = divergence_fractal(mandelbar_function, plot_folder_path, x_range, y_range, x_steps = x_steps, y_steps = y_steps, iterations = iterations)
 
    plt.pcolormesh(real_axis, imag_axis, divergence_time, shading = 'nearest', cmap = 'gist_gray', norm=LogNorm())
    plt.axis('off')
    plt.savefig("{}mandelbar_{}.png".format(plot_folder_path, d), format = 'png', bbox_inches = 'tight', dpi=100)
+   plt.close()
+   return
+   
+def burning_ship(plot_folder_path, d = 2, x_range = [-2, 2], y_range = [-2, 2], x_steps = 101, y_steps = 101, iterations = 100):
+   """
+   Plots the Mandelbar set, or a generalisation with arbitrary exponent d.
+   """
+   if plot_folder_path[-1] != '/':
+      plot_folder_path += '/'
+      
+   burning_ship_function = lambda z : (np.abs(np.real(z)) + 1j * np.abs(np.imag(z)))**d
+   real_axis, imag_axis, divergence_time = divergence_fractal(burning_ship_function, plot_folder_path, x_range, y_range, x_steps = x_steps, y_steps = y_steps, iterations = iterations)
+
+   plt.pcolormesh(real_axis, imag_axis, divergence_time, shading = 'nearest', cmap = 'gist_gray', norm=LogNorm())
+   plt.axis('off')
+   plt.savefig("{}burning_ship_{}.png".format(plot_folder_path, d), format = 'png', bbox_inches = 'tight', dpi=100)
    plt.close()
    return
