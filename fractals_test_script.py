8028eb3320a837537897b5d02cc0e8015aeeed99
Alvin-Gavel <alvin.gavel@gmail.com>
2022-12-21 15:32:56 +0100

    Removed unused option

--- fractals_test_script.py
+++ fractals_test_script.py
@@ -2,17 +2,16 @@ import os
 
 import fractals
 
-sidelength = 1001
-iterations = 100
+sidelength = 10001
+iterations = 1000
 
 plot_folder = 'Fractal_plots'
 try:
    os.mkdir(plot_folder)
 except FileExistsError:
    pass
-for color in [True, False]:
-   fractals.multibrot(plot_folder, d = 2, x_steps = sidelength, y_steps = sidelength, iterations = iterations)
-   fractals.mandelbar(plot_folder, d = 2, x_steps = sidelength, y_steps = sidelength, iterations = iterations)
-   fractals.burning_ship(plot_folder, x_steps = sidelength, y_steps = sidelength, iterations = iterations)
-   # Value of c borrowed from https://commons.wikimedia.org/wiki/File:Julia_set,_plotted_with_Matplotlib.svg
-   fractals.normal_julia(plot_folder, c = -0.512511498387847167 + 0.521295573094847167j, x_steps = sidelength, y_steps = sidelength, iterations = iterations)
+fractals.multibrot(plot_folder, d = 2, x_steps = sidelength, y_steps = sidelength, iterations = iterations)
+fractals.mandelbar(plot_folder, d = 2, x_steps = sidelength, y_steps = sidelength, iterations = iterations)
+fractals.burning_ship(plot_folder, x_steps = sidelength, y_steps = sidelength, iterations = iterations)
+# Value of c borrowed from https://commons.wikimedia.org/wiki/File:Julia_set,_plotted_with_Matplotlib.svg
+fractals.normal_julia(plot_folder, c = -0.512511498387847167 + 0.521295573094847167j, x_steps = sidelength, y_steps = sidelength, iterations = iterations)
