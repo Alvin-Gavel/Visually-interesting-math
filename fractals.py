dceed1b5a9e589c93ef5823dae8a7a32e796c752
Alvin-Gavel <alvin.gavel@gmail.com>
2022-12-21 13:48:49 +0100

    Added julia set

--- fractals.py
+++ fractals.py
@@ -20,32 +20,26 @@ _rgb_array[:,1] = np.linspace(0, 0.9, _length)
 _rgb_array[:,3] = 1
 _YlBl = ListedColormap(_rgb_array, name = 'YlBl')
 
+def _adjust_folder_path(folder_path):
+   if folder_path[-1] != '/':
+      folder_path += '/'
+   return folder_path
+   
+def _adjust_steps(p_range, p_steps):
+   """
+   If we have symmetric intervals, we want to make sure that the real
+   and imaginary axes are plotted.
+   """
+   if abs(p_range[0]) == abs(p_range[1]):
+      p_steps += (p_steps + 1) % 2
+   return p_steps
 
-def divergence_fractal(function, plot_folder_path, x_range, y_range, x_steps = 101, y_steps = 101, iterations = 100):
-   """
-   Given a function f(z), plot which points c on the complex plane diverge
-   under repeated iteration of f_n(z) = f_n-1(z) + c, f_0 = c.
-   """
-   # If we have symmetric intervals, we want to make sure that the real
-   # and imaginary axes are plotted.
-   if abs(x_range[0]) == abs(x_range[1]):
-      x_steps += (x_steps + 1) % 2
-   if abs(y_range[0]) == abs(y_range[1]):
-      y_steps += (y_steps + 1) % 2
-   real_axis = np.linspace(x_range[0], x_range[1], x_steps)
-   imag_axis = np.linspace(y_range[0], y_range[1], y_steps)
+
+def _make_complex_plane(real_axis, imag_axis):
    complex_plane = np.meshgrid(real_axis, imag_axis)
    complex_plane = np.asarray(complex_plane)   
    complex_plane = complex_plane[0,:,:] + 1j * complex_plane[1,:,:]
-   z = np.zeros((x_steps, y_steps), dtype = np.clongdouble)
-   
-   divergence_time = np.ones((x_steps, y_steps), dtype = np.longdouble) * np.inf
-   for i in range(iterations):
-      diverged = np.abs(z) > 2
-      not_diverged = np.logical_not(diverged)      
-      z[not_diverged] = function(z[not_diverged]) + complex_plane[not_diverged]
-      divergence_time[diverged] = np.minimum(divergence_time[diverged], i)
-   return real_axis, imag_axis, divergence_time
+   return complex_plane
 
 def standard_plot(real_axis, imag_axis, divergence_time, plot_path, color = False):
    if color:
@@ -73,8 +67,12 @@ def standard_plot(real_axis, imag_axis, divergence_time, plot_path, color = Fals
       if optimise_width:
          real_convergence_sum = np.where(np.isinf(np.sum(divergence_time, 0)))[0]
          imag_convergence_sum = np.where(np.isinf(np.sum(divergence_time, 1)))[0]
-         x_left, x_right = real_axis[real_convergence_sum[0]], real_axis[real_convergence_sum[-1]]
-         y_low, y_high = imag_axis[imag_convergence_sum[0]], imag_axis[imag_convergence_sum[-1]]
+         if len(real_convergence_sum) != 0:
+            x_left, x_right = real_axis[real_convergence_sum[0]], real_axis[real_convergence_sum[-1]]
+            y_low, y_high = imag_axis[imag_convergence_sum[0]], imag_axis[imag_convergence_sum[-1]]
+         else:
+            print('Converged nowhere!')
+            x_left, x_right, y_low, y_high = real_axis[0], real_axis[-1], imag_axis[0], imag_axis[-1]
       else:
          x_left, x_right, y_low, y_high = real_axis[0], real_axis[-1], imag_axis[0], imag_axis[-1]
       fig.set_size_inches((x_right-x_left)/(y_high-y_low) * approx_screen_height, approx_screen_height)
@@ -89,12 +87,32 @@ def standard_plot(real_axis, imag_axis, divergence_time, plot_path, color = Fals
    plt.close()
    return
 
+def divergence_fractal(function, plot_folder_path, x_range, y_range, x_steps = 101, y_steps = 101, iterations = 100):
+   """
+   Given a function f(z), plot which points c on the complex plane diverge
+   under repeated iteration of f_n(z) = f_n-1(z) + c, f_0 = c.
+   """
+   x_steps = _adjust_steps(x_range, x_steps)
+   y_steps = _adjust_steps(y_range, y_steps)
+   real_axis = np.linspace(x_range[0], x_range[1], x_steps)
+   imag_axis = np.linspace(y_range[0], y_range[1], y_steps)
+   
+   complex_plane = _make_complex_plane(real_axis, imag_axis)
+   z = np.zeros((x_steps, y_steps), dtype = np.clongdouble)
+   divergence_time = np.ones((x_steps, y_steps), dtype = np.longdouble) * np.inf
+   
+   for i in range(iterations):
+      diverged = np.abs(z) > 2
+      not_diverged = np.logical_not(diverged)      
+      z[not_diverged] = function(z[not_diverged]) + complex_plane[not_diverged]
+      divergence_time[diverged] = np.minimum(divergence_time[diverged], i)
+   return real_axis, imag_axis, divergence_time
+
 def multibrot(plot_folder_path, d = 2, x_range = [-2, 2], y_range = [-2, 2], x_steps = 101, y_steps = 101, iterations = 100, color = False):
    """
    Plots the Mandelbrot set, or a generalisation with arbitrary exponent d.
    """
-   if plot_folder_path[-1] != '/':
-      plot_folder_path += '/'
+   plot_folder_path = _adjust_folder_path(plot_folder_path)
    
    multibrot_function = lambda z : z**d
    real_axis, imag_axis, divergence_time = divergence_fractal(multibrot_function, plot_folder_path, x_range, y_range, x_steps = x_steps, y_steps = y_steps, iterations = iterations)
@@ -105,8 +123,7 @@ def mandelbar(plot_folder_path, d = 2, x_range = [-2, 2], y_range = [-2, 2], x_s
    """
    Plots the Mandelbar set, or a generalisation with arbitrary exponent d.
    """
-   if plot_folder_path[-1] != '/':
-      plot_folder_path += '/'
+   plot_folder_path = _adjust_folder_path(plot_folder_path)
       
    mandelbar_function = lambda z : np.conjugate(z)**d
    real_axis, imag_axis, divergence_time = divergence_fractal(mandelbar_function, plot_folder_path, x_range, y_range, x_steps = x_steps, y_steps = y_steps, iterations = iterations)
@@ -117,10 +134,31 @@ def burning_ship(plot_folder_path, x_range = [-2, 2], y_range = [-2, 2], x_steps
    """
    Plots the Burning ship fractal
    """
-   if plot_folder_path[-1] != '/':
-      plot_folder_path += '/'
+   plot_folder_path = _adjust_folder_path(plot_folder_path)
       
    burning_ship_function = lambda z : (np.abs(np.real(z)) + 1j * np.abs(np.imag(z)))**2
    real_axis, imag_axis, divergence_time = divergence_fractal(burning_ship_function, plot_folder_path, x_range, y_range, x_steps = x_steps, y_steps = y_steps, iterations = iterations)
    standard_plot(real_axis, imag_axis, divergence_time, "{}burning_ship".format(plot_folder_path), color = color)
+   return
+   
+def normal_julia(plot_folder_path, c = 0.0, x_range = [-2, 2], y_range = [-2, 2], x_steps = 101, y_steps = 101, iterations = 100, color = False):
+   """
+   Using the default value of c will produce a unit circle.
+   """
+   plot_folder_path = _adjust_folder_path(plot_folder_path)
+   
+   x_steps = _adjust_steps(x_range, x_steps)
+   y_steps = _adjust_steps(y_range, y_steps)
+   real_axis = np.linspace(x_range[0], x_range[1], x_steps)
+   imag_axis = np.linspace(y_range[0], y_range[1], y_steps)
+   z = _make_complex_plane(real_axis, imag_axis)
+   divergence_radius = 1./2 + np.sqrt(1./4 + np.sqrt(c) + np.abs(z))
+
+   divergence_time = np.ones((x_steps, y_steps), dtype = np.longdouble) * np.inf      
+   for i in range(iterations):
+      diverged = np.abs(z) > divergence_radius
+      not_diverged = np.logical_not(diverged)      
+      z[not_diverged] = z[not_diverged]**2 + c
+      divergence_time[diverged] = np.minimum(divergence_time[diverged], i)
+   standard_plot(real_axis, imag_axis, divergence_time, "{}normal_julia".format(plot_folder_path), color = color)
    return
--- fractals_test_script.py
+++ fractals_test_script.py
@@ -13,3 +13,5 @@ for color in [True, False]:
    fractals.multibrot(plot_folder, d = 2, x_steps = sidelength, y_steps = sidelength, color = color)
    fractals.mandelbar(plot_folder, d = 2, x_steps = sidelength, y_steps = sidelength, color = color)
    fractals.burning_ship(plot_folder, x_steps = sidelength, y_steps = sidelength, color = color)
+   # Value of c borrowed from https://commons.wikimedia.org/wiki/File:Julia_set,_plotted_with_Matplotlib.svg
+   fractals.normal_julia(plot_folder, c = -0.512511498387847167 + 0.521295573094847167j, x_steps = sidelength, y_steps = sidelength, color = color)
