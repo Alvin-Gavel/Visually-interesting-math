936f2207fbf11153c18207ff4586206b4a42a2c0
Alvin-Gavel <alvin.gavel@gmail.com>
2023-02-25 18:52:27 +0100

    Added more plot modes

--- star_polygons_test_script.py
+++ star_polygons_test_script.py
@@ -2,10 +2,23 @@ import os
 
 import star_polygons as sp
 
-folder = 'Star_polygons_plots'
-try:
-   os.mkdir(folder)
-except FileExistsError:
-   pass
+main_folder = 'Star_polygons_plots'
+default_folder = main_folder + '/Default'
+shaded_folder = main_folder + '/Shaded'
+inscribed_folder = main_folder + '/Inscribed'
+inwards_folder = main_folder + '/Inwards'
+outwards_folder = main_folder + '/Outwards'
+repetitive_folder = main_folder + '/Repetitions'
+for folder in [main_folder, default_folder, shaded_folder, inscribed_folder, inwards_folder, outwards_folder, repetitive_folder]:
+   try:
+      os.mkdir(folder)
+   except FileExistsError:
+      pass
+
    
-sp.draw_star_polygons_below(10, folder)
+sp.draw_star_polygons_below(10, default_folder, mode = "default")
+sp.draw_star_polygons_below(10, shaded_folder, mode = "shaded")
+sp.draw_star_polygons_below(10, inscribed_folder, mode = "inscribed")
+sp.draw_star_polygons_below(10, inwards_folder, mode = "inwards")
+sp.draw_star_polygons_below(10, outwards_folder, mode = "outwards")
+sp.draw_star_polygons_below(10, repetitive_folder, mode = "default", repetitions = 5)
